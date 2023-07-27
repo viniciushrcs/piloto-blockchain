3193f6625e0ca91771400293d2f82d7e5741338d
ronaldaraujo <f.ronaldaraujo@gmail.com>
2023-07-27 16:55:56 -0300

    feat: Adicionando componente Stepper.

--- client-v2/src/pages/dashboard/index.tsx
+++ client-v2/src/pages/dashboard/index.tsx
@@ -18,7 +18,9 @@ import {
   Center,
   createStyles,
   rem,
-  Select
+  Select,
+  Stepper,
+  Group
 } from '@mantine/core';
 import { hasLength, useForm } from '@mantine/form';
 import {
@@ -30,6 +32,7 @@ import {
   IconMessageDots,
   IconQuestionMark
 } from '@tabler/icons-react';
+import { useEffect, useRef, useState } from 'react';
 
 type FormValues = {
   name: string;
@@ -60,6 +63,17 @@ const useStyles = createStyles((theme) => ({
 export default function Index() {
   const { classes } = useStyles();
 
+  const firstStep = useRef<HTMLButtonElement>(null);
+
+  const stepper = useRef<HTMLDivElement>(null);
+
+  const [active, setActive] = useState(0);
+
+  const nextStep = () =>
+    setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
+
   const form = useForm<FormValues>({
     initialValues: {
       name: '',
@@ -77,15 +91,25 @@ export default function Index() {
     console.log(values);
   };
 
+  useEffect(() => {
+    console.log(active);
+  }, [active]);
+
   return (
     <Container size={'xl'}>
       {/* criar título da página */}
       <Grid>
         <Grid.Col mb={10} md={12}>
           <Title order={1}>Crie sua rede Blockchain</Title>
+          <Group position="center" mt="xl">
+            <Button variant="default" onClick={prevStep}>
+              Back
+            </Button>
+            <Button onClick={nextStep}>Next step</Button>
+          </Group>
         </Grid.Col>
         <Grid.Col md={3}>
-          <Timeline active={-1} bulletSize={24} lineWidth={2}>
+          {/* <Timeline active={-1} bulletSize={24} lineWidth={2}>
             <Timeline.Item
               // CircleCheck
               bullet={<IconCircleDashed size={12} />}
@@ -123,59 +147,75 @@ export default function Index() {
                 A rede foi criada com sucesso!
               </Text>
             </Timeline.Item>
-          </Timeline>
+          </Timeline> */}
+          <Stepper ref={stepper} active={active} orientation="vertical">
+            <Stepper.Step label="First step" ref={firstStep} />
+            <Stepper.Step label="Second step" />
+          </Stepper>
         </Grid.Col>
         <Grid.Col md={9}>
-          <Paper withBorder className="flex-1 p-6 rounded-lg">
-            <div className="space-y-4">
-              <Box component="form" onSubmit={form.onSubmit(onSubmit)}>
-                {/* <LoadingOverlay visible={visible} overlayBlur={1} /> */}
-                <TextInput
-                  withAsterisk
-                  mb="md"
-                  label="Nome da organização"
-                  placeholder="Ex.: Org0"
-                  classNames={classes}
-                  rightSection={
-                    <Tooltip
-                      label="O nome da organização deve ter no mínimo 3 caracteres"
-                      position="top-end"
-                      withArrow
-                      transitionProps={{ transition: 'pop-bottom-right' }}
-                    >
-                      <Text color="dimmed" sx={{ cursor: 'help' }}>
-                        <Center>
-                          <IconInfoCircle size="1.1rem" stroke={1.5} />
-                        </Center>
-                      </Text>
-                    </Tooltip>
-                  }
-                  {...form.getInputProps('name')}
-                />
-                <Select
-                  withinPortal
-                  mb="md"
-                  data={['Sim', 'Não']}
-                  placeholder="Selecione uma opção"
-                  label="É uma organização validadora?"
-                  classNames={classes}
-                  {...form.getInputProps('isValidatingOrganization')}
-                />
-                <Select
-                  withinPortal
-                  mb="md"
-                  data={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-                  placeholder="Selecione uma opção"
-                  label="Número de peers"
-                  classNames={classes}
-                  {...form.getInputProps('numberOfPeers')}
-                />
-                <Button fullWidth mt="xl" size="md" type="submit">
-                  Adicionar
-                </Button>
-              </Box>
-            </div>
-          </Paper>
+          <Box
+            hidden={active !== 0}
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[7]
+                  : theme.colors.gray[0],
+              borderRadius: theme.radius.md,
+              padding: theme.spacing.md,
+              border: `1px solid ${
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[7]
+                  : theme.colors.gray[2]
+              }`
+            })}
+            component="form"
+            onSubmit={form.onSubmit(onSubmit)}
+          >
+            <TextInput
+              withAsterisk
+              mb="md"
+              label="Nome da organização"
+              placeholder="Ex.: Org0"
+              classNames={classes}
+              rightSection={
+                <Tooltip
+                  label="O nome da organização deve ter no mínimo 3 caracteres"
+                  position="top-end"
+                  withArrow
+                  transitionProps={{ transition: 'pop-bottom-right' }}
+                >
+                  <Text color="dimmed" sx={{ cursor: 'help' }}>
+                    <Center>
+                      <IconInfoCircle size="1.1rem" stroke={1.5} />
+                    </Center>
+                  </Text>
+                </Tooltip>
+              }
+              {...form.getInputProps('name')}
+            />
+            <Select
+              withinPortal
+              mb="md"
+              data={['Sim', 'Não']}
+              placeholder="Selecione uma opção"
+              label="É uma organização validadora?"
+              classNames={classes}
+              {...form.getInputProps('isValidatingOrganization')}
+            />
+            <Select
+              withinPortal
+              mb="md"
+              data={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
+              placeholder="Selecione uma opção"
+              label="Número de peers"
+              classNames={classes}
+              {...form.getInputProps('numberOfPeers')}
+            />
+            <Button fullWidth mt="xl" size="md" type="submit">
+              Adicionar
+            </Button>
+          </Box>
         </Grid.Col>
       </Grid>
     </Container>
