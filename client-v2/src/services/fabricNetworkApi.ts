44a48882faa7f64cbb4aa351c7c5e6eb90ff9289
ronaldaraujo <f.ronaldaraujo@gmail.com>
2023-07-29 11:49:47 -0300

    feat: Inserindo variáveis de ambiente e adicionando serviços.

---
+++ client-v2/src/services/fabricNetworkApi.ts
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import {
+  StartNetworkPayload,
+  CreateChannelPayload,
+  DeployChaincodePayload
+} from '@/interfaces/fabricNetworkApiPayloads';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+});
+
+class FabricNetworkApi {
+  async startNetwork(payload: StartNetworkPayload) {
+    try {
+      return await api.post('/start-network', payload);
+    } catch (error) {
+      console.error('Erro ao iniciar a rede:', error);
+      throw error;
+    }
+  }
+
+  async createChannel(payload: CreateChannelPayload) {
+    try {
+      return await api.post('/create-channel', payload);
+    } catch (error) {
+      console.error('Erro ao criar canal:', error);
+      throw error;
+    }
+  }
+
+  deployChaincode = async (payload: DeployChaincodePayload) => {
+    try {
+      return await api.post('/deploy-chaincode', payload);
+    } catch (error) {
+      console.error('Erro ao implantar chaincode', error);
+      throw error;
+    }
+  };
+
+  async getStatus() {
+    try {
+      const response = await api.get('/check-status');
+      return response;
+    } catch (error) {
+      console.error('Erro ao obter o status:', error);
+      throw error;
+    }
+  }
+  async executeChaincode() {
+    try {
+      //todo remover esse hardcode
+      const payload = {
+        ordererOrganization: 'org0',
+        peerOrganizations: [
+          {
+            name: 'org1',
+            peers: ['peer1']
+          }
+        ],
+        channelName: 'channelname',
+        chaincodeName: 'asset-transfer-basic',
+        chaincodePath: 'chaincode-typescript',
+        chaincodeCommand: {
+          init: {
+            Args: ['InitLedger']
+          }
+        }
+      };
+      const response = await api.post('/execute-chaincode', payload);
+      return response;
+    } catch (error) {
+      console.error('Erro ao obter o status:', error);
+      throw error;
+    }
+  }
+}
+
+const FabricNetworkApiInstance = new FabricNetworkApi();
+
+export default FabricNetworkApiInstance;
