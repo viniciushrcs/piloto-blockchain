7481035d4650365164061208da70c04cdc7ec011
ronaldaraujo <f.ronaldaraujo@gmail.com>
2023-08-20 22:27:36 -0300

    feat: Creating config. modal.

--- client-v2/src/stores/organization.ts
+++ client-v2/src/stores/organization.ts
@@ -1,18 +1,23 @@
+/* eslint-disable no-unused-vars */
 import { OrgFormData } from '@/types/orgFormData';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 type State = {
   organizations: OrgFormData[];
-  // eslint-disable-next-line no-unused-vars
   setOrganizations: (organizations: OrgFormData[]) => void;
+  getOrganization: (id: number) => OrgFormData | undefined;
 };
 
 export const useOrganizationStore = create<State>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       organizations: [] as OrgFormData[],
-      setOrganizations: (organizations) => set({ organizations })
+      setOrganizations: (organizations) => set({ organizations }),
+      getOrganization: (id) => {
+        const { organizations } = get();
+        return organizations.find((org) => org.id === id);
+      }
     }),
     {
       name: 'organization-storage'
