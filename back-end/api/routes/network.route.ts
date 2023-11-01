73935b7711220e3ccd4a8b6d63dd47499cb08707
viniciushrcs <viniciushrcs@gmail.com>
2023-11-01 10:30:48 -0300

    fix: check status

--- back-end/api/routes/network.route.ts
+++ back-end/api/routes/network.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { processNetwork, taskStatus } from '../helpers/process-network';
+import { clusterStatus, processNetwork } from '../helpers/process-network';
 import { StartNetworkPayload } from '../interfaces/route-payloads.interface';
 import { up } from '../network-ops';
 import { generateArtifacts } from '../helpers/generate-artifacts';
@@ -15,21 +15,30 @@ router.post('/start-cluster', (req, res) => {
   res.status(202).send('Sua solicitação foi recebida e está sendo processada');
 });
 
-router.get('/check-status', (req, res) => {
-  res.status(200).json(taskStatus);
+router.get('/check-cluster-status', (req, res) => {
+  res.status(200).json(clusterStatus);
 });
 
-router.post('/create-fabric-network', (req, res) => {
+router.get('/check-network-status', (req, res) => {
+  res.status(200).json(networkStatus);
+});
+
+const networkStatus = {
+  inProgress: false,
+  message: '',
+  networkId: '',
+};
+
+router.post('/create-fabric-network', async (req, res) => {
   const specs: StartNetworkPayload = req.body;
   const networkId = generateRandomId();
   process.env.NETWORK_NAME = networkId;
-  generateArtifacts(specs.ordererOrganization, specs.peerOrganizations).then(
-    () => {
-      up(specs).catch((err) => {
-        console.error('Erro ao criar a rede fabric:', err);
-      });
-    }
-  );
+  networkStatus.message = 'Gerando os artefatos';
+  await generateArtifacts(specs.ordererOrganization, specs.peerOrganizations);
+  networkStatus.message = 'Iniciando rede Fabric';
+  await up(specs);
+  networkStatus.networkId = networkId;
+  networkStatus.message = '';
 
   res.status(200).send('Sua solicitação foi recebida e está sendo processada');
 });
