280743ce1a3321bd79e7c9e692ca6fbfa13d1437
viniciushrcs <viniciushrcs@gmail.com>
2023-11-07 11:15:47 -0300

    refactor: network creation

--- back-end/api/routes/network.route.ts
+++ back-end/api/routes/network.route.ts
@@ -1,14 +1,30 @@
 import express from 'express';
-import { clusterStatus, processNetwork } from '../helpers/process-network';
+import { clusterStatus, createCluster } from '../helpers/create-cluster';
+import {
+  networkStatus,
+  createFabricNetwork,
+} from '../helpers/create-fabric-network';
 import { StartNetworkPayload } from '../interfaces/route-payloads.interface';
-import { up } from '../network-ops';
-import { generateArtifacts } from '../helpers/generate-artifacts';
-import { generateRandomId } from '../helpers/helpers';
+import { executeCommand } from '../helpers/helpers';
 
 const router = express.Router();
 
+router.get('/network-ids', (req, res) => {
+  const kubectlCommand = [
+    "kubectl get configmap network-ids -o=jsonpath='{.data.networks}'",
+  ];
+  executeCommand(kubectlCommand)
+    .then((networks) => {
+      const parsedNetworks = networks.split(',');
+      res.status(200).json(parsedNetworks);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 router.post('/start-cluster', (req, res) => {
-  processNetwork().catch((err) => {
+  createCluster().catch((err) => {
     console.error('Erro ao inicializar o cluster:', err);
   });
 
@@ -23,24 +39,12 @@ router.get('/check-network-status', (req, res) => {
   res.status(200).json(networkStatus);
 });
 
-const networkStatus = {
-  inProgress: false,
-  message: '',
-  networkId: '',
-};
-
 router.post('/create-fabric-network', async (req, res) => {
   const specs: StartNetworkPayload = req.body;
-  const networkId = generateRandomId();
-  process.env.NETWORK_NAME = networkId;
-  networkStatus.message = 'Gerando os artefatos';
-  await generateArtifacts(specs.ordererOrganization, specs.peerOrganizations);
-  networkStatus.message = 'Iniciando rede Fabric';
-  await up(specs);
-  networkStatus.networkId = networkId;
-  networkStatus.message = '';
-
-  res.status(200).send('Sua solicitação foi recebida e está sendo processada');
+  createFabricNetwork(specs).catch((err) => {
+    console.error('Erro ao processar a rede:', err);
+  });
+  res.status(202).send('Sua solicitação foi recebida e está sendo processada');
 });
 
 export default router;
